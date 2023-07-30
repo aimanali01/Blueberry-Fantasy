c730fa3daf90c51a5370f1c4e1871798166424e2
Aiman <aimanali122007@gmail.com>
2023-07-30 23:17:36 +0500

    Added app.run

--- main.py
+++ main.py
@@ -297,4 +297,6 @@ def add_cache_control(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
-    return response
+    return response  
+
+app.run()
